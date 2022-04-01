5c2c8b2ab0cef4de2b3985e5780df9c6875ed0f9
roopashree375 <62794413+roopashree375@users.noreply.github.com>
2022-04-01 20:30:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Container,Row,Col} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Table extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      isLoading: false,
+      isError: false
+    }
+  }
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    if (response.ok) {
+      const users = await response.json()
+      this.setState({ users, isLoading: false })
+    } else {
+      this.setState({ isError: true, isLoading: false })
+    }
+  }
+  renderTableHeader = () => {
+    return Object.keys(this.state.users[0]).map(attr => <th key={attr}>{attr.toUpperCase()}</th>)
+  }
+  renderTableRows = () => {
+    return this.state.users.map(user => {
+      return (
+        // <Row>
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.username}</td>
+          <td>{user.email}</td>
+          <td>{`${user.address.street}, ${user.address.city}`}</td>
+          <td>{user.phone}</td>
+          <td>{user.website}</td>
+          <td>{user.company.name}</td>
+        </tr>
+        // </Row>
+      )
+    })
+  }
+  render() {
+    const { users, isLoading, isError } = this.state
+
+    if (isLoading) {
+      return <div>Loading...</div>
+    }
+
+    if (isError) {
+      return <div>Error</div>
+    }
+
+    return users.length > 0
+      ? (
+          <Row>
+            <Col md={6}>
+            <table>
+          <thead>
+            <tr>
+              {this.renderTableHeader()}
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTableRows()}
+          </tbody>
+        </table>
+            </Col>
+            <Col></Col>
+          </Row>
+      ) : (
+        <div>
+          No users.
+      </div>
+      )
+  }
 }
 
-export default App;
+export default Table
